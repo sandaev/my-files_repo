964376dca6bb014d58e8cb40bd4c9892cce8c899
Sanni <sandae20v@gmail.com>
2023-11-14 22:52:40 +0100

    my main.c for testing

--- alx/main.c
+++ alx/main.c
@@ -1,13 +1,31 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Tests code
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-	int num = 12456789;
-	int n;
+	int len, len2;
 
-	n = print_int(num);
-	_putchar('\n');
-	_putchar('0' + n);
-	_putchar('\n');
+	/**
+	 * i = _printf("Sanni Ozigi. I am %i years old. in bits: %b\n", 35, 98);
+	printf("%d\n", i);
+	*/
+
+	_printf("Character: [%c]\n", 'H');
+	printf("Character: [%c]\n", 'H');
+
+	_printf("String: [%s]\n", "I am a string !");
+	printf("String: [%s]\n", "I am a string !");
+
+	len = _printf("Percent: [%%]\n");
+	len2 = printf("Percent: [%%]\n");
+
+	_printf("Len: [%d]\n", len);
+	printf("Len: [%d]\n", len2);
 
 	return (0);
 }
