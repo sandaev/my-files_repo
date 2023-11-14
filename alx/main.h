f6687a6cc6a2fb9a358d48d5332528a656fe1dd4
sandaev <sandaev20@gmail.com>
2023-11-14 04:57:31 +0100

    header

---
+++ alx/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+
+int _strlen(char *s);
+int _putchar(char c);
+int _strlenc(const char *s);
+int printchar(va_list chr);
+int printstr(va_list str);
+int print_perc(void);
+int print_int(va_list nums);
+int print_dec(va_list nums);
+int _printf(const char *format, ...);
+int _exp(int, int);
+int digits(int);
+/*int print_int(int);*/
+
+#endif /* MAIN_H */
