318b2cab1b38c3599d5eedde701e59b284f4d6ad
sandaev <sandaev20@gmail.com>
2023-11-15 12:32:22 -0800

    Hexadecimal func

--- alx/print_hex.c
+++ alx/print_hex.c
@@ -1,146 +1,87 @@
-#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
 
 /**
- * print_hex - Prints the octal representation of a number
+ * put_hex - Fills array
  *
- * @nums: Number
+ * @hexs: Array
+ * @c: value
+ * @i: Index
  *
- * Return: Number of digits
+ * Return: Nothing
  */
-
-int print_hex(va_list nums)
+void put_hex(int hexs[], int c, int i)
 {
-	unsigned int num = va_arg(nums, unsigned int);
-	int i, j, z;
-	int hex[64];
-
-	i = 0;
-	while (num / 16)
+	if (c >= 0 && c <= 9)
 	{
-		z = num % 16;
-		if (z >= 0 || z <= 9)
-			hex[i] = num % 16;
-		else if (z == 10)
-			hex[i] = 'a';
-		else if (z == 11)
-			hex[i] = 'b';
-		else if (z == 12)
-			hex[i] = 'c';
-		else if (z == 13)
-			hex[i] = 'd';
-		else if (z == 14)
-			hex[i] = 'e';
-		else if (z == 15)
-			hex[i] = 'f';
-		num /= 16;
-		i++;
+		hexs[i] = c;
 	}
-	hex[i] = num;
-
-	for (j = i; j >= 0; j--)
+	else if (c == 10)
 	{
-		if (hex[j] > ('0' + hex[j]))
-				write(1, &hex[j], 1);
-		else
-			_putchar('0' + hex[j]);
+		hexs[i] = 'a';
 	}
-
-	return (++i);
+	else if (c == 11)
+	{
+		hexs[i] = 'b';
+	}
+	else if (c == 12)
+	{
+		hexs[i] = 'c';
+	}
+	else if (c == 13)
+	{
+		hexs[i] = 'd';
+	}
+	else if (c == 14)
+	{
+		hexs[i] = 'e';
+	}
+	else if (c == 15)
+	{
+		hexs[i] = 'f';
+	}
 }
 
 
 /**
- * print_heX - Prints the octal representation of a number
- *
- * @nums: Number
+ * print_hex - Prints unsigned int as hex value
+ * @nums: Number to convert
  *
  * Return: Number of digits
  */
-
-int print_heX(va_list nums)
+int print_hex(unsigned int nums)
 {
-	unsigned int num = va_arg(nums, unsigned int);
-	int i, j, z;
-	int hex[64];
+	unsigned int num;
+	int j, i, c;
+	char x;
+	int hexs[64];
 
 	i = 0;
+	num = nums;
+
 	while (num / 16)
 	{
-		z = num % 16;
-		if (z >= 0 && z <= 9)
-			hex[i] = num % 16;
-		else if (z == 10)
-			hex[i] = 'A';
-		else if (z == 11)
-			hex[i] = 'B';
-		else if (z == 12)
-			hex[i] = 'C';
-		else if (z == 13)
-			hex[i] = 'D';
-		else if (z == 14)
-			hex[i] = 'E';
-		else if (z == 15)
-			hex[i] = 'F';
+		c = num % 16;
+		put_hex(hexs, c, i);
 		num /= 16;
 		i++;
 	}
-	hex[i] = num;
+	hexs[i] = num;
 
 	for (j = i; j >= 0; j--)
 	{
-		if (hex[j] > ('0' + hex[j]))
-			write (1, &hex[j], 1);
+		if (hexs[j] >= 97)
+		{
+			x = hexs[j];
+			write(1, &x, 1);
+		}
 		else
-			_putchar('0' + hex[j]);
+		{
+			c = '0' + hexs[j];
+			write(1, &c, 1);
+		}
 	}
+	printf("\n");
 
-	return (++i);
-}
-/**
- * print_hexf - Prints the octal representation of a number preceded with 0
- *
- * @nums: Number
- *
- * Return: Number of digits
- */
-
-int print_hexf(va_list nums)
-{
-	
-	unsigned int num = va_arg(nums, unsigned int);
-	int i, j, z;
-	int hex[64];
-
-	i = 0;
-	_putchar('0');
-	_putchar('x');
-	i += 2;
-	while (num / 16)
-	{
-		z = num % 16;
-		if (z >= 0 && z <= 9)
-			hex[i] = num % 16;
-		else if (z == 10)
-			hex[i] = 'a';
-		else if (z == 11)
-			hex[i] = 'b';
-		else if (z == 12)
-			hex[i] = 'c';
-		else if (z == 13)
-			hex[i] = 'd';
-		else if (z == 14)
-			hex[i] = 'e';
-		else if (z == 15)
-			hex[i] = 'f';
-		num /= 16;
-		i++;
-	}
-	hex[i] = num;
-
-	for (j = i; j >= 0; j--)
-	{
-		_putchar('0' + hex[j]);
-	}
-
-	return (++i);
+	return (i);
 }
